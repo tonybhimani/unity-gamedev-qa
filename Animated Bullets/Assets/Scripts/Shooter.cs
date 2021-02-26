fedf04a7ac0ee150cae490042d412125e299a51d
Tony Bhimani <tonybhimani@users.noreply.github.com>
2021-02-25 18:01:50 -0800

    Add files via upload
    Animated Bullets
    
    Questions comes from my Mega Man series Mambu tutorial. Enhancing the BulletScript to allow for animated bullets.
    
    Made with Unity 2019.3.10f1

---
+++ Animated Bullets/Assets/Scripts/Shooter.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour
+{
+    int bulletIndex;
+    [SerializeField] GameObject bulletPrefab;
+    [SerializeField] BulletScript.BulletTypes bulletType;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // default to the first bullet
+        bulletType = BulletScript.BulletTypes.Bullet1;
+        bulletIndex = (int)bulletType;
+
+        // shooting object we're on
+        ShowShootingObjectName();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // create and shoot the object with left mouse click
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject bullet = Instantiate(bulletPrefab);
+            bullet.name = bulletPrefab.name;
+            bullet.transform.position = transform.position;
+            bullet.GetComponent<BulletScript>().SetBulletSpeed(1f);
+            bullet.GetComponent<BulletScript>().SetBulletDirection(Vector2.right);
+            bullet.GetComponent<BulletScript>().SetBulletType(bulletType);
+            bullet.GetComponent<BulletScript>().SetDestroyDelay(5f);
+            bullet.GetComponent<BulletScript>().Shoot();
+            // make it bigger to see it better
+            bullet.transform.localScale *= 2f;
+        }
+
+        // change shooting object via right mouse click
+        if (Input.GetMouseButtonDown(1))
+        {
+            var bulletArray = Enum.GetValues(typeof(BulletScript.BulletTypes));
+            if (++bulletIndex > (int)bulletArray.Length - 1)
+            {
+                bulletIndex = 0;
+            }
+            bulletType = (BulletScript.BulletTypes)bulletIndex;
+
+            // shooting object we're on
+            ShowShootingObjectName();
+        }
+    }
+
+    private void ShowShootingObjectName()
+    {
+        // show the shooting object name on the console
+        Debug.Log("Shooting Object: " + Enum.GetName(typeof(BulletScript.BulletTypes), bulletIndex));
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Unity Game Dev Q&A
-Project solutions for game development questions from my YouTube videos.
+
+Project solutions for game development questions from my YouTube series videos.
+
+The source code for the Q&amp;A solutions is here and this is the YouTube playlist. 
+
+https://www.youtube.com/playlist?list=PLfKMG2CcLNaKmwxEbLrmrEFijOzineod9
+
+If you find this useful or have any questions then you can leave comments on my YouTube channel.
